9d377623da1e1b7fe08e91f6b885188c65b13dd1
Rodrigo B. de Oliveira <rbo@acm.org>
2014-07-07 13:44:14 -0300

    compiler output

--- require.js
+++ require.js
@@ -7,15 +7,15 @@ _$0: {
                 _$4: {
                     ast.defineSymbol(ast.createMacro('#require', 'unary', 'LOW', {
                         'expand': function (ast) {
-                            var specs, decl, nameFromString, expand, _$5, _$6;
+                            var specs, decl, nameFromString, tagToString, expand, _$5, _$6;
                             _$6: {
                                 specs = ast.at(0);
                                 decl = function (tag) {
-                                    var doto2i0wilbfr, _$7, _$8;
+                                    var dotozx7xo43hb, _$7, _$8;
                                     _$8: {
-                                        doto2i0wilbfr = tag.copy();
-                                        doto2i0wilbfr.handleAsTagDeclaration();
-                                        _$7 = doto2i0wilbfr;
+                                        dotozx7xo43hb = tag.copy();
+                                        dotozx7xo43hb.handleAsTagDeclaration();
+                                        _$7 = dotozx7xo43hb;
                                         break _$8;
                                     }
                                     return _$7;
@@ -23,13 +23,16 @@ _$0: {
                                 nameFromString = function (str) {
                                     return str.newTag(require('path').basename(str.val));
                                 };
+                                tagToString = function (t) {
+                                    return t.newValue(t.getTag());
+                                };
                                 expand = function (spec) {
-                                    var tagOrString, refs, name, _$7, _$8, codeTag0, _$9, _$10, _$11, _$12, codeTag1, _$13, _$14, _$15, codeTag2, _$16;
+                                    var tagOrString, refs, name, ref, nameVal, _$7, _$8, codeTag0, _$9, _$10, _$11, _$12, codeTag1, _$13, _$14, _$15, _$16, codeTag2, _$17, _$18, _$19, codeTag3, _$20;
                                     if (spec.isTag()) {
                                         _$8: {
-                                            codeTag0 = ast.fromJsonString('{"id":"=","kind":"builtin","val":"=","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":22,"column":12},"end":{"line":23,"column":8}},"args":[{"id":"<tagDeclaration>","kind":"tag","val":"unquote1","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":22,"column":12},"end":{"line":22,"column":28}},"args":[]},{"id":"<call>","kind":"builtin","val":null,"loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":22,"column":40},"end":{"line":22,"column":75}},"args":[{"id":"<tag>","kind":"tag","val":"require","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":22,"column":32},"end":{"line":22,"column":32}},"args":[]},{"id":"<tag>","kind":"tag","val":"unquote2","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":22,"column":40},"end":{"line":22,"column":75}},"args":[]}]}]}');
+                                            codeTag0 = ast.fromJsonString('{"id":"=","kind":"builtin","val":"=","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":25,"column":12},"end":{"line":26,"column":8}},"args":[{"id":"<tagDeclaration>","kind":"tag","val":"unquote1","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":25,"column":12},"end":{"line":25,"column":28}},"args":[]},{"id":"<call>","kind":"builtin","val":null,"loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":25,"column":40},"end":{"line":25,"column":59}},"args":[{"id":"<tag>","kind":"tag","val":"require","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":25,"column":32},"end":{"line":25,"column":32}},"args":[]},{"id":"<tag>","kind":"tag","val":"unquote2","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":25,"column":40},"end":{"line":25,"column":59}},"args":[]}]}]}');
                                             codeTag0.replaceTag('unquote1', decl(spec));
-                                            codeTag0.replaceTag('unquote2', spec.newValue(spec.getTag()));
+                                            codeTag0.replaceTag('unquote2', tagToString(spec));
                                             _$7 = codeTag0;
                                             break _$8;
                                         }
@@ -45,7 +48,7 @@ _$0: {
                                                 }
                                                 name = _$10;
                                                 _$12: {
-                                                    codeTag1 = ast.fromJsonString('{"id":"<tuple>","kind":"builtin","val":null,"loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":32,"column":12},"end":{"line":34,"column":8}},"args":[{"id":"<tag>","kind":"tag","val":"unquote1","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":32,"column":12},"end":{"line":32,"column":36}},"args":[]},{"id":"refer","kind":"macro","val":"refer","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":33,"column":12},"end":{"line":33,"column":12}},"args":[{"id":"<tag>","kind":"tag","val":"unquote2","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":33,"column":18},"end":{"line":33,"column":25}},"args":[]},{"id":"<tag>","kind":"tag","val":"unquote3","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":33,"column":27},"end":{"line":33,"column":34}},"args":[]}]}]}');
+                                                    codeTag1 = ast.fromJsonString('{"id":"<tuple>","kind":"builtin","val":null,"loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":35,"column":12},"end":{"line":37,"column":8}},"args":[{"id":"<tag>","kind":"tag","val":"unquote1","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":35,"column":12},"end":{"line":35,"column":36}},"args":[]},{"id":"refer","kind":"macro","val":"refer","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":36,"column":12},"end":{"line":36,"column":12}},"args":[{"id":"<tag>","kind":"tag","val":"unquote2","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":36,"column":18},"end":{"line":36,"column":25}},"args":[]},{"id":"<tag>","kind":"tag","val":"unquote3","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":36,"column":27},"end":{"line":36,"column":34}},"args":[]}]}]}');
                                                     codeTag1.replaceTag('unquote1', expand(tagOrString));
                                                     codeTag1.replaceTag('unquote2', name);
                                                     codeTag1.replaceTag('unquote3', refs);
@@ -59,24 +62,46 @@ _$0: {
                                             if (spec.isTuple()) {
                                                 _$7 = spec.newTuple(spec.map(expand));
                                             } else {
-                                                if ('string' === typeof spec.val) {
+                                                if (spec.isProperty()) {
                                                     _$13: {
-                                                        name = nameFromString(spec);
-                                                        _$15: {
-                                                            codeTag2 = ast.fromJsonString('{"id":"=","kind":"builtin","val":"=","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":41,"column":12},"end":{"line":42,"column":8}},"args":[{"id":"<tagDeclaration>","kind":"tag","val":"unquote1","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":41,"column":12},"end":{"line":41,"column":28}},"args":[]},{"id":"<call>","kind":"builtin","val":null,"loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":41,"column":40},"end":{"line":41,"column":40}},"args":[{"id":"<tag>","kind":"tag","val":"require","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":41,"column":32},"end":{"line":41,"column":32}},"args":[]},{"id":"<tag>","kind":"tag","val":"unquote2","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":41,"column":40},"end":{"line":41,"column":40}},"args":[]}]}]}');
-                                                            codeTag2.replaceTag('unquote1', decl(name));
-                                                            codeTag2.replaceTag('unquote2', spec);
-                                                            _$14 = codeTag2;
-                                                            break _$15;
+                                                        ref = spec.at(0);
+                                                        name = spec.at(1);
+                                                        if (name.isTag()) {
+                                                            _$14 = tagToString(name);
+                                                        } else {
+                                                            _$14 = name;
                                                         }
-                                                        _$7 = _$14;
+                                                        nameVal = _$14;
+                                                        _$16: {
+                                                            codeTag2 = ast.fromJsonString('{"id":"=","kind":"builtin","val":"=","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":50,"column":12},"end":{"line":51,"column":8}},"args":[{"id":"<tagDeclaration>","kind":"tag","val":"unquote1","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":50,"column":12},"end":{"line":50,"column":27}},"args":[]},{"id":"<call>","kind":"builtin","val":null,"loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":50,"column":39},"end":{"line":50,"column":39}},"args":[{"id":"<tag>","kind":"tag","val":"require","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":50,"column":31},"end":{"line":50,"column":31}},"args":[]},{"id":"<tag>","kind":"tag","val":"unquote2","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":50,"column":39},"end":{"line":50,"column":39}},"args":[]}]}]}');
+                                                            codeTag2.replaceTag('unquote1', decl(ref));
+                                                            codeTag2.replaceTag('unquote2', nameVal);
+                                                            _$15 = codeTag2;
+                                                            break _$16;
+                                                        }
+                                                        _$7 = _$15;
                                                         break _$13;
                                                     }
                                                 } else {
-                                                    _$16: {
-                                                        spec.error('unsupported #require pattern: ' + spec.printAst());
-                                                        _$7 = null;
-                                                        break _$16;
+                                                    if ('string' === typeof spec.val) {
+                                                        _$17: {
+                                                            name = nameFromString(spec);
+                                                            _$19: {
+                                                                codeTag3 = ast.fromJsonString('{"id":"=","kind":"builtin","val":"=","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":54,"column":12},"end":{"line":55,"column":8}},"args":[{"id":"<tagDeclaration>","kind":"tag","val":"unquote1","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":54,"column":12},"end":{"line":54,"column":28}},"args":[]},{"id":"<call>","kind":"builtin","val":null,"loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":54,"column":40},"end":{"line":54,"column":40}},"args":[{"id":"<tag>","kind":"tag","val":"require","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":54,"column":32},"end":{"line":54,"column":32}},"args":[]},{"id":"<tag>","kind":"tag","val":"unquote2","loc":{"source":"/home/bamboo/code/hyperfair/hash-require/require.mjs","start":{"line":54,"column":40},"end":{"line":54,"column":40}},"args":[]}]}]}');
+                                                                codeTag3.replaceTag('unquote1', decl(name));
+                                                                codeTag3.replaceTag('unquote2', spec);
+                                                                _$18 = codeTag3;
+                                                                break _$19;
+                                                            }
+                                                            _$7 = _$18;
+                                                            break _$17;
+                                                        }
+                                                    } else {
+                                                        _$20: {
+                                                            spec.error('unsupported #require pattern: ' + spec.printAst());
+                                                            _$7 = null;
+                                                            break _$20;
+                                                        }
                                                     }
                                                 }
                                             }
